0b6ce6d1046a28de44366c1d74a807e73f9d7202
Svitlana Belinska <svi.belinska@gmail.com>
2023-08-01 12:52:46 +0200

    started implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { IncomeComponent } from './income/income.component';
+import { HeaderComponent } from './header/header.component';
+import { ViewComponent } from './view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpensesComponent,
+    IncomeComponent,
+    HeaderComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
